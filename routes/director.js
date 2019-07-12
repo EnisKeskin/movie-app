b4cf4bdf94554b02e5f324efc2a198b1f2a62234
enis.keskin <eniskeskin61@hotmail.com>
2019-07-12 16:11:57 +0300

    Added to director.js get and post methods

--- routes/director.js
+++ routes/director.js
@@ -5,8 +5,33 @@ const router = express.Router();
 //Models
 const Director = require('../models/Director');
 
-router.get('/', (req, res, next) => {
-    const promise = Director.find({})
+router.get('/', (req, res) => {
+    const promise = Director.aggregate([{
+        $lookup: {
+            from: 'movies',
+            localField: '_id',
+            foreignField: 'director_id',
+            as: 'movies'
+        }},
+        {$unwind: {
+            path: '$movies',
+            preserveNullAndEmptyArrays: true,
+        }},
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+  
+    ]);
     promise.then((data) => {
         res.json(data);
     }).catch((err) => {
@@ -14,7 +39,47 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.post('/', (req, res, next) => {
+router.get('/:director_id', (req, res) => {
+    const promise = Director.aggregate([
+        {
+            $match: {
+                '_id': mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
+        {
+        $lookup: {
+            from: 'movies',
+            localField: '_id',
+            foreignField: 'director_id',
+            as: 'movies'
+        }},
+        {$unwind: {
+            path: '$movies',
+            preserveNullAndEmptyArrays: true,
+        }},
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+  
+    ]);
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+})
+
+router.post('/', (req, res) => {
     const director = new Director(req.body);
     const promise = director.save();
 
